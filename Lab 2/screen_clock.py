6caab9bf8b765ea0834563c6e8fe919a9a306a15
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-02-28 15:06:20 -0500

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -63,6 +63,15 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+step_count = 0
+while step_count = 0:
+    try:
+        # get a color from the user and convert it to RGB
+        step_count = (input('Type the number of steps you want to hit today, without any commas:'))
+    except ValueError:
+        # catch colors we don't recognize and go again
+        print("whoops I don't know that one")
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
@@ -71,14 +80,14 @@ while True:
     time = "Time is: " + (strftime("%H:%M:%S"))
     coffee_count = ""
     if time > "12:00:00":
-        coffee_count = "You should have had 2 cups of coffee by now"
+        coffee_count = "You should have walked "+ step_count
     else:
         coffee_count = "You should have had 1 cup of coffee by now"
     draw.text((x, y), date, font=font, fill="#FFFFFF")
     y += font.getsize(date)[1]
     draw.text((x, y), time, font=font, fill="#FFFF00")
     y += font.getsize(time)[1]
-    draw.text((x, y), coffee_count, font=font, fill="#FFFF00")
+    draw.text((x, y), coffee_count, font=font, fill="#0000FF")
    
     # Display image.
     disp.image(image, rotation)
