41b0099cbf05e43b34092f2b3a007083500c1f7c
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-03-03 10:40:38 -0500

    Update image.py

--- Lab 2/image.py
+++ Lab 2/image.py
@@ -94,30 +94,27 @@ while True:
         image = Image.open("smile.jpg")  # set the screen to white
     if not buttonA.value and not buttonB.value:  # none pressed
         image = Image.open("red.jpg")  # green
+    #image = Image.open("smile.jpg")
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
 
 
-#image = Image.open("smile.jpg")
-backlight = digitalio.DigitalInOut(board.D22)
-backlight.switch_to_output()
-backlight.value = True
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
-
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
-
-# Display image.
-disp.image(image)
-
+    # Display image.
+    disp.image(image)
