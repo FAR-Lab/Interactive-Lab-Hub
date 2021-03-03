a0f7bcb4195889c81c271082e9bb3790591edf91
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-03-03 11:02:36 -0500

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -21,6 +21,13 @@ BAUDRATE = 64000000
 # Setup SPI bus using hardware SPI:
 spi = board.SPI()
 
+#defining the buttons
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+
 # Create the ST7789 display:
 disp = st7789.ST7789(
     spi,
@@ -69,17 +76,17 @@ step_count = 0
 while step_count == 0:
     try:
         # get a color from the user and convert it to RGB
-        step_count = int(input('Type the number of steps you want to hit today, without any commas:'))
+        step_count = int(input('Type in your Step Goal for today, without any commas:'))
     except ValueError:
         # catch colors we don't recognize and go again
-        print("whoops I don't know that one")
+        print("whoops that is not a number")
 
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     y = top
-    date = "Date is: " + (strftime("%m/%d/%Y")) #displaying the date in an easy to read fashion
-    time = "Time is: " + (strftime("%H:%M:%S"))    #displaying the time in an easy to read fashion
+    date = "Today is: " + (strftime("%m/%d/%Y")) #displaying the date in an easy to read fashion
+    time = "The time is: " + (strftime("%H:%M:%S"))    #displaying the time in an easy to read fashion
     if time > "08:00:00":
         diff = (datetime.datetime.now().hour) - 8 #assuming that a person wakes up at 8am, this is the number of hours they've been awake
     else:
@@ -92,6 +99,11 @@ while True:
     draw.text((x, y), time, font=font, fill="#FFFF00")
     y += font.getsize(time)[1]
     draw.text((x, y), step_count_text, font=font, fill="#0000FF")
+    y += font.getsize(step_count_text)[1]
+    draw.text((x, y), "Press A if you're on track", font=font, fill="#0000FF")
+    y += font.getsize(time)[1]
+    draw.text((x, y), "Press B if you're falling behind", font=font, fill="#0000FF")
+    
    
     # Display image.
     disp.image(image, rotation)
