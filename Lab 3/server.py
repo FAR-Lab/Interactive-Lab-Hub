781432c5db7055a17aefc956b27f3f0782d4b659
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-03-17 11:15:44 -0400

    Update server.py

--- Lab 3/server.py
+++ Lab 3/server.py
@@ -7,6 +7,6 @@ def index():
     return 'Hello world'
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='http://ixe00.local:5000')
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='http://aa653.local:5000')
+    #app.run(debug=True, host='0.0.0.0')
 
