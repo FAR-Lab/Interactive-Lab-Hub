add83b2c84d05aff455792561e056151974578f8
iamyuchy <iamyuchy@gmail.com>
2021-03-22 15:10:13 -0400

    Update pitest.py
    update flow

--- Lab 3/pitest.py
+++ Lab 3/pitest.py
@@ -7,49 +7,55 @@ import wave
 import smbus, time
 from screen_test import draw_text
 
-# if not os.path.exists("model"):
-#     print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
-#     exit (1)
 
+model = Model("model")
+rec = KaldiRecognizer(model, wf.getframerate(), "start test hi hello zero oh one two three four five six seven eight nine [unk]")
 
-# wf = wave.open("temp.wav", "rb")
-# model = Model("model")
-# rec = KaldiRecognizer(model, wf.getframerate(), "start test hi hello zero oh one two three four five six seven eight nine [unk]")
-
-# record_command = "record -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav temp.wav"
 
 # #JoyStick Control
 bus = smbus.SMBus(1)
 addr = 0x20
 
-# while True:
-
-#     data = wf.readframes(4000)
-#     if len(data) == 0:
-#         break
-#     if rec.AcceptWaveform(data):
-#         print(rec.Result())
-#     else:
-#         print(rec.PartialResult())
 
 def main():
-	global bus_data,X,Y,mode
-	while True:
-    	# If joystick got pressed
-		if not qwiicjoystick():
-        		mode = 0
-        		draw_text(mode)
+    global bus_data,X,Y,mode
+    mode = -1
+    while True:
+        # If joystick got pressed
+        if not qwiicjoystick():
+                mode = 0
+                draw_text(mode)
+                start_listen()
+
+
+def start_listen():
+    record_command = "record -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav temp.wav"
+    os.system(record_command)
+    time.sleep(1)
+    wf = wave.open("temp.wav", "rb")
+    data = wf.readframes(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        res = rec.Result()
+        if(res):
+            mode = 1
+            draw_text(mode)
+        #print(rec.Result())
+    else:
+        #print(rec.PartialResult())
+
 
 
 def qwiicjoystick():
-	try:
-    		bus_data = bus.read_i2c_block_data(addr, 0x03, 5)
-	except Exception as e:
-        	print(e)
-	X = (bus_data[0]<<8 | bus_data[1])>>6
-	Y = (bus_data[2]<<8 | bus_data[3])>>6
-	selected = bus_data[4]
-	return selected
+    try:
+            bus_data = bus.read_i2c_block_data(addr, 0x03, 5)
+    except Exception as e:
+            print(e)
+    X = (bus_data[0]<<8 | bus_data[1])>>6
+    Y = (bus_data[2]<<8 | bus_data[3])>>6
+    selected = bus_data[4]
+    return selected
 
 if __name__ == '__main__':
     main()
