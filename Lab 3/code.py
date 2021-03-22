a17d2bcad8937454befaaa3f55ee50fd28988eee
nicole-zy <78866283+nicole-zy@users.noreply.github.com>
2021-03-22 14:14:54 -0400

    Update code.py

--- Lab 3/code.py
+++ Lab 3/code.py
@@ -1,1 +1,51 @@
 
+from time import strftime, sleep
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+import qwiic_button
+import random 
+
+
+
+#subprocess.call("~/Interactive-Lab-Hub/Lab\ 3/im.py", shell=True)
+ # joystick = qwiic_joystick.QwiicJoystick()
+import digitalio
+import board
+from PIL import Image, ImageDraw
+import adafruit_rgb_display.ili9341 as ili9341
+import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
+import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+  
+  button = qwiic_button.QwiicButton()
+button.has_been_clicked
+if button.begin() == False:
+        print("\nThe Qwiic Button isn't connected to the system. Please check your connection")
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
