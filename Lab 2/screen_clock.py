564502d6123978094d08c7993171659266418221
Heidi <heidi3.1415926@gmail.com>
2021-03-22 02:52:36 -0400

    update feature

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -100,7 +100,6 @@ while True:
     if buttonA.value:  # just button A pressed
         # display.fill(screenColor) # set the screen to the users color
         # backlight.value = False  # turn off backlight
-        draw.text((0, top), "Press Button\n To See Clock", font=font, fill="#FFFF00")
         curTime = strftime("%H:%M:%S")
 
         curHour = int(strftime("%H"))
@@ -109,13 +108,19 @@ while True:
         unitWidth = disp.width/4
         unitHeight = disp.height/6
         # print(unitWidth, unitHeight)
-
+        draw.rectangle( (0,0, width, height), fill =(255,255,255), outline =(255,255,255))
         for i in range(curHour):
             shape = [(i%6)*unitHeight,(i//6)*unitWidth,(i%6)*unitHeight+unitHeight,(i//6)*unitWidth+unitWidth]
             if i<8 or i>20:
-                draw.rectangle( shape, fill =(0,0,180), outline =(255,255,255))
+                draw.rectangle( shape, fill =(255,255,180), outline =(255,255,255))
             else:
-                draw.rectangle( shape, fill =(i*10,i*10,0), outline =(255,255,255))
+                draw.rectangle( shape, fill =(i*10,i*10,255), outline =(255,255,255))
+        draw.text((0, top), "Press Button\n To See Clock", font=font, fill="#FFFF00")
+
+    elif buttonB.value:
+        curDate = strftime("%m/10/%Y")
+        draw.text((0, top), curTime, font=font, fill="#0000FF")
+
     else:
         # image = Image.new("RGB", (width, height))
         # backlight.value = True  # turn off backlight
