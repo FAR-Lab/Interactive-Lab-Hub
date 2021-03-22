4d00ead92dbf8ceb56c793bc13c6b1af3addae23
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-03-22 10:44:09 -0400

    Update project.py

--- Lab 3/demo/project.py
+++ Lab 3/demo/project.py
@@ -110,12 +110,17 @@ while True:
         #    pass
     if buttonA.value and not buttonB.value:  # just button B pressed
         print(count)
-        if count == 1:
+        if count == 0:
             speak2me("Alright, let's go to the fridge. The first step is to turn left and walk three steps.")
+            #count = count + 1
+        if count == 1:
+            speak2me("Ok now turn left and walk five steps")
             count = count + 1
         else:
             pass
-        speak2me("Ok now turn left and walk five steps")
+        count = count + 1
+        #speak2me("Ok now turn left and walk five steps")
+        
         #speak2me("Button B pressed")
         print("in here2")
     else:
