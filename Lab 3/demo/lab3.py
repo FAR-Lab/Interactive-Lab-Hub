f2eaf89fd80d7947972eefd98c622db32832b6ed
Ross Kleinrock <rmk328@cornell.edu>
2021-03-22 12:12:32 -0400

    lab 3 final

--- Lab 3/demo/lab3.py
+++ Lab 3/demo/lab3.py
@@ -3,38 +3,12 @@ import qwiic_joystick
 import time
 import sys
 from subprocess import Popen, call
-import wave
+
 
 
 def speak2me(val):
     call(f"espeak '{val}'", shell=True)
 
-'''
-def Speech2Text():
-    wf = wave.open("recording.wav", "rb")
-    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-        print ("Audio file must be WAV format mono PCM.")
-        exit (1)
-
-    model = Model("model")
-    # You can also specify the possible word list
-    rec = KaldiRecognizer(model, wf.getframerate(), "east west middle snow")
-
-    while True:
-        data = wf.readframes(4000)
-        if len(data) == 0:
-            break
-        if rec.AcceptWaveform(data):
-            print(rec.Result())
-        else:
-            print(rec.PartialResult())
-    res = json.loads(rec.FinalResult())
-    print ("Speech2Text: "+ res['text'])
-    return res['text']
-
-text = Speech2Text()
-print(text)
-'''
 
 def swing():
 
@@ -49,9 +23,9 @@ def swing():
             speak2me("Your ball is in the right rough")
         if ((myJoystick.get_vertical() > 205) and (myJoystick.get_vertical() <= 409)):
             speak2me("Your ball is in the right side of the fairway")
-        if ((myJoystick.get_vertical() > 409) and (myJoystick.get_vertical() <= 501)):
+        if ((myJoystick.get_vertical() > 409) and (myJoystick.get_vertical() <= 500)):
             speak2me("Your ball is in the middle of the fairway")
-        if ((myJoystick.get_vertical() > 505) and (myJoystick.get_vertical() <= 614)):
+        if ((myJoystick.get_vertical() > 506) and (myJoystick.get_vertical() <= 614)):
             speak2me("Your ball is in the middle of the fairway")
         if ((myJoystick.get_vertical() > 614) and (myJoystick.get_vertical() <= 818)):
             speak2me("Your Ball is in the left side of the fairway")
@@ -67,7 +41,7 @@ def swing():
                     myJoystick.get_button()))
         '''
 
-        time.sleep(.5)
+        time.sleep(2)
 
 if __name__ == '__main__':
     try:
