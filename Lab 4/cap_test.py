0014bd1787fac37c2d069391872ec4f37d668fd2
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-03-31 10:44:18 -0400

    Update cap_test.py

--- Lab 4/cap_test.py
+++ Lab 4/cap_test.py
@@ -2,8 +2,7 @@ import time
 import board
 import busio
 import digitalio
-import PIL
-from PIL import Image, ImageDraw
+import Image, ImageDraw
 import adafruit_rgb_display.ili9341 as ili9341
 import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
 import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
