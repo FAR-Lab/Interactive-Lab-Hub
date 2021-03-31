b2ca13f6173385b5f93b99c4d2a277a64e8ad4f7
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-03-31 10:46:28 -0400

    Update image.py

--- Lab 4/image.py
+++ Lab 4/image.py
@@ -83,15 +83,15 @@ disp.image(image)
 #new code
 while True:
     if buttonA.value and buttonB.value:
-        image = Image.open("red.jpg")  # turn off backlight
+        image = Image.open("call.png")  # turn off backlight
     else:
-        image = Image.open("red.jpg")  # turn on backlight
+        image = Image.open("call.png")  # turn on backlight
     if buttonB.value and not buttonA.value:  # just button A pressed
-        image = Image.open("red.jpg") # set the screen to image
+        image = Image.open("call.png") # set the screen to image
     if buttonA.value and not buttonB.value:  # just button B pressed
-        image = Image.open("smile.jpg")  # set the screen to white
+        image = Image.open("call.png")  # set the screen to white
     if not buttonA.value and not buttonB.value:  # none pressed
-        image = Image.open("red.jpg")  # green
+        image = Image.open("call.png")  # green
     #image = Image.open("smile.jpg")
     backlight = digitalio.DigitalInOut(board.D22)
     backlight.switch_to_output()
