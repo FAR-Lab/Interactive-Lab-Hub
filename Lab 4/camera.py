40b9acc997a42e24121ee194050d2e8fbd246450
William Zhang <williamzhang012998@gmail.com>
2021-04-03 18:25:25 -0400

    Experiment4

--- Lab 4/camera.py
+++ Lab 4/camera.py
@@ -21,7 +21,7 @@ while True:
             RESET_PIN = digitalio.DigitalInOut(board.D4)
 
             i2c = board.I2C()
-            oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3D, reset=RESET_PIN)
+            oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
             # Clear display.
             oled.fill(0)
