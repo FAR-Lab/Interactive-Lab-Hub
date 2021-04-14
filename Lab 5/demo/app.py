fixed the averaging because of annoying global variable stuff in flask

Lab 5/demo/app.py
@@ -27,7 +27,7 @@ hardware = 'plughw:2,0'
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-acc_history = list(np.zeros((10, 3)))
+acc_history = list(np.zeros((10,3)))
 
 @socketio.on('connect')
 def test_connect():
@@ -36,13 +36,14 @@ def test_connect():
 
 @socketio.on('ping-gps')
 def handle_message(val):
+    global acc_history
     acc_history = acc_history[1:]
     acc_history += [list(mpu.acceleration)]
     if sum(np.where(np.array(mpu.acceleration) > 10)) > 0:
         emit('thresh-passed', {'data': 'BOAT!!!'})
     else:
         emit('thresh-passed', {'data': ''})
-    emit('pong-gps', np.mean(acc_history, axis=0)) 
+    emit('pong-gps', tuple(np.mean(acc_history, axis=0))) 
 
 
 