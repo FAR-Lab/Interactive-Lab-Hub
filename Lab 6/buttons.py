aef5684ea2a14e77a06aab7422dc9b0368f1ebde
William Zhang <williamzhang012998@gmail.com>
2021-05-05 16:26:25 -0400

    Buttons

--- Lab 6/buttons.py
+++ Lab 6/buttons.py
@@ -4,25 +4,19 @@ import time
 import board
 import busio
 from subprocess import Popen, call
-import digitalio
-
 
 
 import paho.mqtt.client as mqtt
 import uuid
 
-cs_pin = digitalio.DigitalInOut(board.CE0)
-dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = None
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
-# these setup the code for our buttons and the backlight and tell the pi to treat the GPIO pins as digitalIO vs analogIO
-backlight = digitalio.DigitalInOut(board.D22)
-backlight.switch_to_output()
-backlight.value = True
-buttonA = digitalio.DigitalInOut(board.D23)
-buttonB = digitalio.DigitalInOut(board.D24)
-buttonA.switch_to_input()
-buttonB.switch_to_input()
+buttonPin_R = 18
+buttonPin_G = 23
+
+GPIO.setup(buttonPin_R, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(buttonPin_G, GPIO.IN, GPIO.PUD_UP)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -31,23 +25,24 @@ read_topic = 'IDD/foodserv/food'
 
 
 def on_connect(client, userdata, flags, rc):
-        print(f"connected with result code {rc}")
-        client.subscribe(read_topic)
-        # you can subsribe to as many topics as you'd like
-        # client.subscribe('some/other/topic')
+	print(f"connected with result code {rc}")
+	client.subscribe(read_topic)
+	# you can subsribe to as many topics as you'd like
+	# client.subscribe('some/other/topic')
 
 
 # this is the callback that gets called each time a message is recived
 def on_message(cleint, userdata, msg):
-        message = msg.payload.decode('UTF-8')
-        print(f"topic: {msg.topic} msg: {message}")
-        call(f"espeak -s125 '{message}'", shell=True)
+	message = msg.payload.decode('UTF-8')
+	print(f"topic: {msg.topic} msg: {message}")
+	call(f"espeak -s125 '{message}'", shell=True)
 
 client = mqtt.Client(str(uuid.uuid1()))
 # configure network encryption etc
 client.tls_set()
 # this is the username and pw we have setup for the class
 client.username_pw_set('idd', 'device@theFarm')
+
 
 client.on_connect = on_connect
 client.on_message = on_message
@@ -60,12 +55,12 @@ client.connect(
 # client.loop_forever()
 
 while True:
-        client.loop()
-        if buttonA.value:
-                buttonSts = 'Do not touch my food'
-                client.publish(send_topic, buttonSts)
-                # call(f"espeak -s125 '{buttonSts}'", shell=True)
-        if buttonB.value:
-                buttonSts = 'Go ahead and enjoy it'
-                client.publish(send_topic, buttonSts)
-                # call(f"espeak -s125 '{buttonSts}'", shell=True)
+	client.loop()
+	if GPIO.input(buttonPin_R) == 1:
+		buttonSts = 'Do not touch my food'
+		client.publish(send_topic, buttonSts)
+		# call(f"espeak -s125 '{buttonSts}'", shell=True)
+	if GPIO.input(buttonPin_G) == 1:
+		buttonSts = 'Go ahead and enjoy it'
+		client.publish(send_topic, buttonSts)
+		# call(f"espeak -s125 '{buttonSts}'", shell=True)
