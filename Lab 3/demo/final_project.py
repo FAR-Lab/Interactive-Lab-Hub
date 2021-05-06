0eb9d2d2aca4484a1ca01ef773c0dbc3fe8f707f
nagrawal44 <46833072+nagrawal44@users.noreply.github.com>
2021-05-06 11:58:34 -0400

    Update final_project.py

--- Lab 3/demo/final_project.py
+++ Lab 3/demo/final_project.py
@@ -21,7 +21,7 @@ from queue import Queue
 #from adafruit_apds9960.apds9960 import APDS9960
 
 i2c = busio.I2C(board.SCL, board.SDA)
-mpu = adafruit_mpu6050.MPU6050(i2c)
+#mpu = adafruit_mpu6050.MPU6050(i2c)
 int_pin = digitalio.DigitalInOut(board.D5)
 #apds = APDS9960(i2c, interrupt_pin=int_pin)
 
