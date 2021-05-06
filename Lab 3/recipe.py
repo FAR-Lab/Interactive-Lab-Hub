40b106dbff518d953dcc760f3235762b09045711
Heidi <heidi3.1415926@gmail.com>
2021-05-06 03:10:13 -0400

    update file

--- Lab 3/recipe.py
+++ Lab 3/recipe.py
@@ -6,6 +6,7 @@ import adafruit_rgb_display.st7789 as st7789
 import webcolors
 
 import pygame
+import os
 # The display uses a communication protocol called SPI.
 # SPI will not be covered in depth in this course.
 # you can read more https://www.circuitbasics.com/basics-of-the-spi-communication-protocol/
@@ -36,15 +37,26 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 
-
 # get a color from the user
 display.fill(color565(0, 255, 0))
 
 pygame.mixer.init()
-pygame.mixer.music.load("audios/Step1.aac")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+
+# states = ["step1", "step2", "step3", "step4", "step5"]
+curstate = 0
+musics=[]
+#iterate files in audios
+for filename in os.listdir("audios"):
+    if filename.endswith(".wav"):
+        curmusic = pygame.mixer.music.load("audios/%s"%filename)
+        musics.append[curmusic]
+        print('find audio %s'%filname)
+
+
+# pygame.mixer.music.load("audios/Step1.wav")
+# pygame.mixer.music.play()
+# while pygame.mixer.music.get_busy() == True:
+#     continue
 
 # Main loop:
 while True:
@@ -53,8 +65,19 @@ while True:
     else:
         backlight.value = True  # turn on backlight
     if buttonB.value and not buttonA.value:  # just button A pressed
-        display.fill(screenColor) # set the screen to the users color
+        if curstate>0:
+            curstate = curstate - 1
+        print(curstate)
     if buttonA.value and not buttonB.value:  # just button B pressed
-        display.fill(color565(255, 255, 255))  # set the screen to white
-    if not buttonA.value and not buttonB.value:  # none pressed
-        display.fill(color565(0, 255, 0))  # green
+        if curstate<5:
+            curstate = curstate + 1
+        print(curstate)
+
+    # if not buttonA.value and not buttonB.value:  # none pressed
+    #     display.fill(color565(0, 255, 0))  # green
+    if curstate == 0:
+        continue
+    else:
+        musics[curstate-1].play()
+        while musics[curstate-1].get_busy() == True:
+            continue
