89f6d0fa0fe3e99925dcf93a0de4c14b2e35eff5
Heidi <heidi3.1415926@gmail.com>
2021-05-09 02:03:20 -0400

    update detection

--- Lab 5/runModel.py
+++ Lab 5/runModel.py
@@ -6,10 +6,50 @@ import numpy as np
 import cv2
 import sys
 
+import digitalio
+import board
 
+from adafruit_rgb_display.rgb import color565
+import adafruit_rgb_display.st7789 as st7789
+import webcolors
+
+import pygame
+import os
+
+# ----------------
+# enable button on adafruit adafruit_rgb_display
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+BAUDRATE = 64000000  # the rate  the screen talks to the pi
+# Create the ST7789 display:
+display = st7789.ST7789(
+    board.SPI(),
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+
+# these setup the code for our buttons and the backlight and tell the pi to treat the GPIO pins as digitalIO vs analogIO
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
+display.fill(color565(0, 255, 0))
 
+# -----------------
+# Load camera
 img = None
 camera = False
 
@@ -35,16 +75,14 @@ for line in f.readlines():
     labels.append(line.split(' ')[1].strip())
 
 
-print
-
-while(True):
+def detectKnot():
     if camera:
         ret, img = cap.read()
 
     rows, cols, channels = img.shape
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    image = Image.open('/home/pi/openCV-examples/data/test.jpg')
+    # image = Image.open('/home/pi/openCV-examples/data/test.jpg')
     size = (224, 224)
     img =  cv2.resize(img, size, interpolation = cv2.INTER_AREA)
     #turn the image into a numpy array
@@ -58,17 +96,30 @@ while(True):
     # run the inference
     prediction = model.predict(data)
     print("I think its a:",labels[np.argmax(prediction)])
+    cv2.imwrite('detected_out.jpg',img)
 
-    if webCam:
-        if sys.argv[-1] == "noWindow":
-           cv2.imwrite('detected_out.jpg',img)
-           continue
-        cv2.imshow('detected (press q to quit)',img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cap.release()
-            break
-    else:
+print("press up button to start detection")
+while(True):
+
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        print("start detecting knot")
+        detectKnot()
+        print("press button again if need another detection")
+
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        cap.release() #quit
         break
 
-cv2.imwrite('detected_out.jpg',img)
-cv2.destroyAllWindows()
+
+# while(True):
+#
+#     if camera:
+#         if sys.argv[-1] == "noWindow":
+#            cv2.imwrite('detected_out.jpg',img)
+#            continue
+#         cv2.imshow('detected (press q to quit)',img)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             cap.release()
+#             break
+#     else:
+#         break
