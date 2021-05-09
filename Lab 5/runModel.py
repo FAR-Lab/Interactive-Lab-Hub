2d179529c9b38166eb0b3744fdb032a5abab5d9c
Heidi <heidi3.1415926@gmail.com>
2021-05-09 02:15:50 -0400

    fix error

--- Lab 5/runModel.py
+++ Lab 5/runModel.py
@@ -36,7 +36,8 @@ display = st7789.ST7789(
     x_offset=53,
     y_offset=40,
 )
-
+height = disp.width
+width = disp.height
 
 # these setup the code for our buttons and the backlight and tell the pi to treat the GPIO pins as digitalIO vs analogIO
 backlight = digitalio.DigitalInOut(board.D22)
