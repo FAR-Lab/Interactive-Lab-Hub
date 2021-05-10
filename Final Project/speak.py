c8608605bc6081e631100651159bc216ff72cf23
iamyuchy <iamyuchy@gmail.com>
2021-05-10 01:43:42 -0400

    Update speak.py

--- Final Project/speak.py
+++ Final Project/speak.py
@@ -8,6 +8,18 @@ def say(text):
     print(str(text))
     engine.runAndWait()
 
+def speak(mode):
+	if(mode == 1):
+		say("Oh hey, hello!")
+	elif(mode == 2):
+		say("Who's touching me!")
+	elif(mode == 3):
+		say("Do you want to know the room tempeture and humidity?")
+	elif(mode == 4):
+		say("Anyway I will just let you know! Since this is the only thing I know! The current tempeture is ...")
+	else:
+		pibot_face = sleep_emoticon
+
 if __name__ == '__main__':
 	t = "test test"
 	say(t)
