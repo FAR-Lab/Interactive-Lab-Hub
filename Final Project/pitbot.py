b20685f121c5e0b51a8eab827c06d9ad6599e570
iamyuchy <iamyuchy@gmail.com>
2021-05-10 01:13:06 -0400

    Update pitbot.py

--- Final Project/pitbot.py
+++ Final Project/pitbot.py
@@ -1,11 +1,25 @@
 from display import draw_pibot
+from Qwiic_Joystick import qwiicjoystick()
+
+import smbus, time
+bus = smbus.SMBus(1)
+addr = 0x20
 
 SLEEPMODE = 0
 WAKEUPMODE = 1
 ROTMODE = 2
 QUESTIONMODE = 3
 RESULTMODE = 4
+
 if __name__ == '__main__':
 	mode = SLEEPMODE
+	global bus_data, X, Y
+	
 	while True:
-		draw_pibot(1)
+		direction, selected = qwiicjoystick()
+		if !selected:
+			if mode == RESULTMODE:
+				mode = SLEEPMODE
+			else:
+				mode += 1
+			draw_pibot(mode)
