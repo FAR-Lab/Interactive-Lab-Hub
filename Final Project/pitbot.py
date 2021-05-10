6bdf3f6eed9db1565e129a5f6438ec21563118a5
iamyuchy <iamyuchy@gmail.com>
2021-05-10 01:14:29 -0400

    Update pitbot.py

--- Final Project/pitbot.py
+++ Final Project/pitbot.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 	
 	while True:
 		direction, selected = qwiicjoystick()
-		if selected == 1:
+		if selected == 0:
 			if mode == RESULTMODE:
 				mode = SLEEPMODE
 			else:
