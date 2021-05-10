e9bb9898d97ec3c2747f2ec047ecf4a12e1e7557
iamyuchy <iamyuchy@gmail.com>
2021-05-10 15:15:59 -0400

    Update pitbot.py

--- Final Project/pitbot.py
+++ Final Project/pitbot.py
@@ -2,16 +2,34 @@ from display import draw_pibot
 from Qwiic_Joystick import qwiicjoystick
 from speak import speak_by_mode
 from servo import oneturn, setup, destroy
-
+import board
+import busio
+import adafruit_mpr121
 import smbus, time
+
 bus = smbus.SMBus(1)
 addr = 0x20
+
+i2c = busio.I2C(board.SCL, board.SDA)
+mpr121 = adafruit_mpr121.MPR121(i2c)
 
 SLEEPMODE = 0
 WAKEUPMODE = 1
 ROTMODE = 2
 QUESTIONMODE = 3
 RESULTMODE = 4
+
+def act(mode):
+	if mode == RESULTMODE:
+		mode = SLEEPMODE
+	else:
+		mode += 1
+	draw_pibot(mode)
+	if mode == ROTMODE:
+		setup()
+		oneturn()
+	speak_by_mode(mode)
+	return mode
 
 if __name__ == '__main__':
 	mode = SLEEPMODE
@@ -21,14 +39,8 @@ if __name__ == '__main__':
 	while True:
 		direction, selected = qwiicjoystick()
 		if selected == 0:
-			if mode == RESULTMODE:
-				mode = SLEEPMODE
-			else:
-				mode += 1
-			draw_pibot(mode)
-			if mode == ROTMODE:
-				setup()
-				oneturn()
-			speak_by_mode(mode)
+			mode = act(mode)
+		if mpr121[6].value:
+			mode = act(mode)
 	destroy()
 	
