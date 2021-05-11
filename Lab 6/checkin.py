7aff7b035c96554aabd73ae345e41db8752135d8
Heidi <heidi3.1415926@gmail.com>
2021-05-11 04:06:23 -0400

    update

--- Lab 6/checkin.py
+++ Lab 6/checkin.py
@@ -2,6 +2,9 @@
 
 import paho.mqtt.client as mqtt
 import uuid
+from subprocess import call
+from datetime import datetime
+
 
 # Every client needs a random ID
 client = mqtt.Client(str(uuid.uuid1()))
@@ -15,11 +18,41 @@ client.connect(
     'farlab.infosci.cornell.edu',
     port=8883)
 
-val = "whether pressed button"
-# while True:
-client.publish("IDD/checkin", val)
 
 
+
+def check_time():
+    now = datetime.now()
+
+    current_time = now.strftime("%H")
+    print("Current Time =", current_time)
+
+    attime = False
+    if current_time == "04":
+        attime = True
+
+    return attime
+
+
+
+def report():
+    cmd_beg= 'espeak '
+    cmd_end= ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
+    cmd_out= '--stdout > /home/pi/Desktop/Text.wav ' # To store the voice file
+
+    text = "today weather is"
+
+    #Replacing ' ' with '_' to identify words in the text entered
+    text = text.replace(' ', '_')
+
+    #Calls the Espeak TTS Engine to read aloud a Text
+    call([cmd_beg+cmd_out+text+cmd_end], shell=True)
+
+while True:
+    if check_time():
+        report()
+        val = "pressed"
+        client.publish("IDD/checkin", val)
 # while True:
 # 	cmd = input('>> topic: IDD/')
 # 	if ' ' in cmd:
