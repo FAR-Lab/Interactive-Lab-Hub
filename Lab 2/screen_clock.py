cc69ea80a2d6fab43a7b54f46ef09b580f1b8bc2
standardnormal <avs57@cornell.edu>
2021-09-16 13:27:10 -0400

    functioning clock, no fun stuff yet

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -65,7 +65,9 @@ while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
+    Time=time.strftime("%m/%d/%Y %H:%M:%S")
+    y=top
+    draw.text((x,y),Time,font=font,fill='#A033FF')
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
