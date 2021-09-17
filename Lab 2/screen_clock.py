da0db202e17eeb76826f8230a3bac32a36d0edbd
WEIWENLAN <weiwenlan1999@gmail.com>
2021-09-17 10:20:11 +0800

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -66,6 +66,11 @@ while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: fill in here. You should be able to look in cli_clock.py and stats.py 
+    from time import strftime, sleep
+    while True:
+        print (strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True)
+        print("\r", end="", flush=True)
+        sleep(1)
 
     # Display image.
     disp.image(image, rotation)
