b6a913109ce800e9c57392b0d2d0252c3f206c18
WEI <weiwenlan1999@gmail.com>
2021-09-19 23:16:47 -0400

    1

--- Lab 2/clock_part1.py
+++ Lab 2/clock_part1.py
@@ -1,7 +1,7 @@
 '''
 Author: Wenlan Wei
 Date: 2021-09-19 21:18:28
-LastEditTime: 2021-09-19 23:06:47
+LastEditTime: 2021-09-19 23:16:36
 LastEditors: Please set LastEditors
 Description: Part1 Homework Lab2 A simple Customize Clock
 FilePath: /Interactive-Lab-Hub/Lab 2/clock_part1.py
@@ -124,8 +124,8 @@ while True:
         n = n+1
         print(image.size)
 
-        image = image.rotate(90)
-        print(image.size)
+        image_2 = image.rotate(90)
+        print(image_2.size)
         image = image.resize((240, 135))
         print(image.size)
         disp.image(image)
