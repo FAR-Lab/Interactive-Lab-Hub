d71e81f79cf9b63291429f8232b2a06d91fb1e2c
WEI <weiwenlan1999@gmail.com>
2021-09-20 16:35:25 -0400

    1

--- Lab 2/clock_part1.py
+++ Lab 2/clock_part1.py
@@ -90,88 +90,88 @@ buttonB.switch_to_input()
 current_time = time.strftime("%H:%M")
 
 
+# while True:
+#     # Draw a black filled box to clear the image.
+#     # draw.rectangle((0, 0, width, height), outline=0, fill=0)
+#     # y = top
+#     # draw.text((x, y), current_time, font=font, fill="#FFFFFF")
+#     # disp.image(image, rotation)
+#     # time.sleep(1)
+
+# TODO: fill in here. You should be able to look in cli_clock.py and stats.py
+
+n = 1
+t = 0
+interval = 1
+SHOW = False
+
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    y = top
-    draw.text((x, y), current_time, font=font, fill="#FFFFFF")
-    disp.image(image, rotation)
-    time.sleep(1)
 
-    # TODO: fill in here. You should be able to look in cli_clock.py and stats.py
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
 
-    n = 1
-    t = 0
-    interval = 1
-    SHOW = False
+    if n >= 8:
+        n = 1
+        t = 0
 
-    while True:
+    if buttonA.value and not buttonB.value:
+        SHOW = True
 
-        # Create blank image for drawing.
-        # Make sure to create image with mode 'RGB' for full color.
+    elif buttonB.value and not buttonA.value:
+        n = 1
+        t = 0
+        SHOW = False
 
-        if n >= 8:
-            n = 1
-            t = 0
+    if SHOW == False:
+        REMIND = "Press Button A to start Timing"
+        draw.text((0, -2), REMIND, font=font, fill="#FFFFFF")
+        break
+    else:
+        if disp.rotation % 180 == 90:
+            height = disp.width  # we swap height/width to rotate it to landscape!
+            width = disp.height
 
-        if buttonA.value and not buttonB.value:
-            SHOW = True
-
-        elif buttonB.value and not buttonA.value:
-            n = 1
-            t = 0
-            SHOW = False
-
-        if SHOW == False:
-            REMIND = "Press Button A to start Timing"
-            draw.text((0, -2), REMIND, font=font, fill="#FFFFFF")
-            break
         else:
-            if disp.rotation % 180 == 90:
-                height = disp.width  # we swap height/width to rotate it to landscape!
-                width = disp.height
+            width = disp.width  # we swap height/width to rotate it to landscape!
+            height = disp.height
 
-            else:
-                width = disp.width  # we swap height/width to rotate it to landscape!
-                height = disp.height
+        image = Image.new("RGB", (width, height))
 
-            image = Image.new("RGB", (width, height))
+        # Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
+        pictureName = 'images/'+"test"+str(n)+".jpg"
+        image = Image.open(pictureName)
+        n = n+1
 
-            # Get drawing object to draw on image.
-            draw = ImageDraw.Draw(image)
-            pictureName = 'images/'+"test"+str(n)+".jpg"
-            image = Image.open(pictureName)
-            n = n+1
+        image_2 = image.transpose(Image.ROTATE_90)
+        image = image_2.resize((135, 240))
+        disp.image(image)
+        time.sleep(interval)
+        t += interval
+        print("time pass", str(t), "second")
+    # backlight = digitalio.DigitalInOut(board.D22)
+    # backlight.switch_to_output()
+    # backlight.value = True
 
-            image_2 = image.transpose(Image.ROTATE_90)
-            image = image_2.resize((135, 240))
-            disp.image(image)
-            time.sleep(interval)
-            t += interval
-            print("time pass", str(t), "second")
-        # backlight = digitalio.DigitalInOut(board.D22)
-        # backlight.switch_to_output()
-        # backlight.value = True
+    # # Scale the image to the smaller screen dimension
+    # image_ratio = image.width / image.height
+    # screen_ratio = width / height
 
-        # # Scale the image to the smaller screen dimension
-        # image_ratio = image.width / image.height
-        # screen_ratio = width / height
+    # if screen_ratio < image_ratio:
+    #     scaled_width = image.width * height // image.height
+    #     scaled_height = height
+    # else:
+    #     scaled_width = width
+    #     scaled_height = image.height * width // image.width
+    # image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
-        # if screen_ratio < image_ratio:
-        #     scaled_width = image.width * height // image.height
-        #     scaled_height = height
-        # else:
-        #     scaled_width = width
-        #     scaled_height = image.height * width // image.width
-        # image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
-
-        # # Crop and center the image
-        # x = scaled_width // 2 - width // 2
-        # y = scaled_height // 2 - height // 2
-        # image = image.crop((x, y, x + width, y + height))
-        # # Display image.
-        # disp.image(image)
-        # time.sleep(4)
+    # # Crop and center the image
+    # x = scaled_width // 2 - width // 2
+    # y = scaled_height // 2 - height // 2
+    # image = image.crop((x, y, x + width, y + height))
+    # # Display image.
+    # disp.image(image)
+    # time.sleep(4)
 
     # Display image.
     #disp.image(image, rotation)
