be69282715790c1f5c62aac9ad6a12e5c8ae5a9e
Andrew S <89856906+standardnormal@users.noreply.github.com>
2021-09-26 13:53:25 -0400

    first test of new event clock

--- Lab 2/screen_clock2.py
+++ Lab 2/screen_clock2.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import subprocess
 import digitalio
 import board
@@ -60,16 +61,74 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+    
+
+_cal={}
+no_events="No more events today."
+with open(calendar.json) as f:
+    _cal=json.load(f)
+   
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-    Time=int(time.strftime("%S"))+int(time.strftime("%M"))*60+60*60*int(time.strftime("%H"))
-    IDD_len=round(Time/(75*60),4)
-    IDD_time = str(IDD_len)+" IDD classes\npast midnight."
-    y=top
-    draw.text((x,y),IDD_time,font=font,fill='#A033FF')
-    # Display image.
+    _time=int(time.strftime("%M"))+60*int(time.strftime("%H"))
+   # while input_mode=0:
+        if buttonA.value and buttonB.value: #both buttons pressed
+            #backlight.value = False  # turn off backlight
+        else if buttonB.value and not buttonA.value:  # just button B pressed
+            #display.fill(screenColor) # set the screen to the users color
+        else if buttonA.value and not buttonB.value:  # just button A pressed
+            # input_mode=1
+            #display.fill(color565(255, 255, 255))  # set the screen to white
+        else if not buttonA.value and not buttonB.value:  # none pressed
+            current_event=""
+            next_event=""
+            # Determine which event is current and which event is next:
+            for date in _cal:
+                if date==datetime.today():
+                    for event in _cal[date]:
+                        _event=_cal[date][event]
+                        if _time < int(time.strftime(_event["end"],"%M")+60*int(time.strftime(_event["end"],"%H"):                # if current time is before event's end time
+                            if _time>= int(time.strftime(_event["start"],"%M")+60*int(time.strftime(_event["start"],"%H"):        # if current time is after event's start time
+                                current_event=_event
+                            if _time < int(time.strftime(_event["start"],"%M")+60*int(time.strftime(_event["start"],"%H"):        # if current time is before event's start time
+                                next_event=_event
+                        if next_event!="": break
+             # Write out default text
+             if current_event!="":
+                current_time = str(int(time.strftime(_event["end"],"%M")+60*int(time.strftime(_event["end"],"%H"-_time)+" minutes left in\n"+current_event["name"]
+                y=top
+                draw.text((x,y),current_time,font=font,fill='#A033FF')
+                y += 2*font.getsize(current_time)[1]
+             if next_event!="":
+                next_time = str(int(time.strftime(_event["start"],"%M")+60*int(time.strftime(_event["start"],"%H"-_time)+" minutes until\n"+next_event["name"]
+                draw.text((x,y),next_time,font=font,fill='#A033FF')
+             else if next_event=="":
+                draw.text((x,y),no_events,font=font,fill='#A033FF')
     disp.image(image, rotation)
-    time.sleep(1)
+    time.sleep(1)    
+                                                                                      
+    
+ """ 
+ def new_event():
+    print("Please enter your new calendar event below:")
+    event_name=input("What is the name of the event?")
+    if event_name='': return input_mode=0
+    new_date=input("Enter a date (MM/DD/YYYY): ")
+    if new_date='': return input_mode=0
+    time_start=input("Enter a start time (HH:MM): ")
+    if time_start='': return input_mode=0
+    time_end=input("Enter an end time (HH:MM): ")
+    if time_end='': return input_mode=0
+   
+ input_mode=0
+ 
+    if input_mode=1:
+ """
+                       
+                       
+                       
+                       
+                       
+                       
+                       
