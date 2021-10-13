23fe74d6dc7190089cca673bd484b8a51b10aeaf
Andrew S <89856906+standardnormal@users.noreply.github.com>
2021-10-13 11:33:57 -0400

    Update scriptread.py

--- Lab 3/scriptread.py
+++ Lab 3/scriptread.py
@@ -7,6 +7,11 @@ with open("script.json") as f:
 os.system("flite -voice slt -v 'Which part will you play?'")
 comp_part=input("Device's part: ")
 
+os.system("flite -voice slt -v 'Would you like to start or should I?'")
+_start=int(input("Who starts? 0 for User, 1 for Device: "))
+if _start==0: os.system("flite -voice slt -v 'Begin when you are ready'")
+if _start==1: os.system("flite -voice slt -v 'Which line should I start with?'")
+
 while True:
   num=int(input("Line: "))
   line=_script[comp_part][num]
