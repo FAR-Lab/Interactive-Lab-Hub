3ec58b3d34d9a712605aafe77c95af44e40fa04e
rohangreddy <rohanreddy34@gmail.com>
2021-10-13 18:29:56 -0400

    adding lab 3 updates

---
+++ Lab 3/covid/covid_test.sh
@@ -0,0 +1,12 @@
+#https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
+
+#!/bin/bash
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say $*
+say "Have you been tested for COVID this week?"
+
+
+#arecord -f cd -r 16000 -d 5 -t wav recorded.wav && sox recorded.wav recorded_mono.wav remix 1,2
+
+arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 test_words.py recorded_mono.wav
