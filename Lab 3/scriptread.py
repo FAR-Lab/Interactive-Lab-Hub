e6bef0b9ccf483018bfef001dc5e7cfa0cbf5e7a
Andrew S <89856906+standardnormal@users.noreply.github.com>
2021-10-13 10:44:29 -0400

    Update scriptread.py

--- Lab 3/scriptread.py
+++ Lab 3/scriptread.py
@@ -5,6 +5,10 @@ import os
 with open("script.json") as f:
   _script=json.load(f)
   
-line=_script["Romeo"][0]
 
-os.system("flite -voice slt -v "+line)
+
+
+while True:
+  num=input("Line: ")
+  line=_script["Romeo"][num]
+  os.system("flite -voice slt -v "+line)
