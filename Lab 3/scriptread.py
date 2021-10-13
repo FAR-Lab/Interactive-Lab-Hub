81bc764e2b7a5cf5d7e2f264177b7d1e9dfb525e
Andrew S <89856906+standardnormal@users.noreply.github.com>
2021-10-13 11:22:13 -0400

    Update scriptread.py

--- Lab 3/scriptread.py
+++ Lab 3/scriptread.py
@@ -3,8 +3,11 @@ import os
 
 with open("script.json") as f:
   _script=json.load(f)
+  
+os.system("flite -voice slt -v 'Which part will you play?'")
+comp_part=input("Device's part: ")
 
 while True:
   num=int(input("Line: "))
-  line=_script["Romeo"][num]
+  line=_script[comp_part][num]
   os.system("flite -voice slt -v "+line)
