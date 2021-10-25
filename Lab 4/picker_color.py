57ac35ab6f6744eb3330bf57eb9a7c4dbe88788e
Andrew S <89856906+standardnormal@users.noreply.github.com>
2021-10-25 10:07:11 -0400

    Update picker_color.py

--- Lab 4/picker_color.py
+++ Lab 4/picker_color.py
@@ -5,11 +5,71 @@ import time
 import board
 from adafruit_apds9960.apds9960 import APDS9960
 from adafruit_apds9960 import colorutility
+import subprocess
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+from colormap import rgb2hex
 
 i2c = board.I2C()
 apds = APDS9960(i2c)
 apds.enable_color = True
 
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
 
 while True:
     # create some variables to store the color data in
@@ -20,11 +80,19 @@ while True:
 
     # get the data and print the different channels
     r, g, b, c = apds.color_data
-    print("red: ", r)
-    print("green: ", g)
-    print("blue: ", b)
-    print("clear: ", c)
+    _hex=rgb2hex(r,g,b)
+    _red="Red: "+r
+    _green="Green: "+g
+    _blue="Blue: "+b
+    y = top
+    draw.text((x, y), _red, font=font, fill="#FF0000")
+    y += font.getsize(_red)[1]
+    draw.text((x, y), _green, font=font, fill="#1E300")
+    y += font.getsize(_red)[1]
+    draw.text((x, y), _blue, font=font, fill="#0050FF")
+    y += font.getsize(_blue)[1]
+    draw.text((x, y), "Final Color: "+_hex, font=font, fill=_hex)
 
-    print("color temp {}".format(colorutility.calculate_color_temperature(r, g, b)))
-    print("light lux {}".format(colorutility.calculate_lux(r, g, b)))
+    #print("color temp {}".format(colorutility.calculate_color_temperature(r, g, b)))
+    #print("light lux {}".format(colorutility.calculate_lux(r, g, b)))
     time.sleep(0.5)
