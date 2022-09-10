080f6b09b46c82e8f5a5859f3208001781b731a9
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-10 19:48:41 -0400

    fix bug

--- Lab 2/chess_clock.py
+++ Lab 2/chess_clock.py
@@ -75,10 +75,10 @@ while True:
     draw.rectangle((0, 0, 120, height), outline=0, fill="#ED4242")
     
     y = top
-    draw.text((x, y), "Height: " + str(height) + ", Width: " + str(width), font=font, fill="#875AFF")
+    # draw.text((x, y), "Height: " + str(height) + ", Width: " + str(width), font=font, fill="#875AFF")
 
     if buttonB.value and not buttonA.value:  # just button A pressed
-        draw.text((x, y), str(buttonB.value), font=font, fill="#875AFF") # set the screen to the users color
+        draw.text((x, y), "HELLO", font=font, fill="#875AFF") # set the screen to the users color
 
     # create starting point
 
