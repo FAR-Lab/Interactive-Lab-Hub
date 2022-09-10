a5b802d6195f6cbc32d113cb7b169d5dded1f4a3
Patrick Mazza <pmm259@cornell.edu>
2022-09-10 15:32:35 -0400

    hourglass shape

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -60,11 +61,35 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    time2 = (strftime("%m/%d/%Y %H:%M:%S"))
+    y = top
+    
+  
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+
+
+    #draw.text((x, y), pokemon, font=font, fill="#FFFFFF")
+    draw.line((120 , 64.5, 208, 22.5 ), fill="#FFFFFF", width=1)
+    draw.line((120 , 70.5, 208, 108.5 ), fill="#FFFFFF", width=1)
+    draw.line((208, 22.5, 224, 22.5 ), fill="#FFFFFF", width=1)
+    draw.line((208, 108.5, 224, 108.5 ), fill="#FFFFFF", width=1)
+    draw.line((224, 108.5, 224, 22.5 ), fill="#FFFFFF", width=1)
+
+
+    draw.line((32 , 22.5, 120, 64.5 ), fill="#FFFFFF", width=1)
+    draw.line((32 , 108.5, 120, 70.5 ), fill="#FFFFFF", width=1)
+    draw.line((32 , 22.5, 16, 22.5 ), fill="#FFFFFF", width=1)
+    draw.line((32, 108.5, 16, 108.5), fill="#FFFFFF", width=1)
+    draw.line((16, 22.5, 16, 108.5), fill="#FFFFFF", width=1)
+
+   # draw.polygon([(224, 22.5), (224, 108.5), (208, 108.5), (120, 70.5), (120, 64.5), (108, 22.5)], fill="#FFFFFF")
+    y += font.getsize(time2)[1]
+    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
+
 
     # Display image.
     disp.image(image, rotation)
