a528a0254827a9b64ca316f28af293fcf3cca87a
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-10 19:44:19 -0400

    change to chess clock

--- Lab 2/chess_clock.py
+++ Lab 2/chess_clock.py
@@ -63,13 +63,13 @@ backlight.value = True
 
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill="#CCA701")
+    draw.rectangle((0, 0, width, height), outline=0, fill="#191919")
 
-    # create a grid
-    draw.rectangle((0, 0, width, 40), outline=0, fill="#ED4242")
+    # create 2 panels
+    draw.rectangle((0, 0, 120, height), outline=0, fill="#ED4242")
+    
     y = top
-
-    draw.text((x, y), "Height: " + str(height) + ", Width: " + str(width), font=font, fill="#FFFFFF")
+    draw.text((x, y), "Height: " + str(height) + ", Width: " + str(width), font=font, fill="#875AFF")
 
     # create starting point
 
