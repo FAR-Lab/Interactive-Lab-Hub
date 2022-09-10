fc312d3c8be49d8d00607f3a0836e2257e7cb5b9
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-10 19:22:16 -0400

    convert to str

--- Lab 2/snake_time.py
+++ Lab 2/snake_time.py
@@ -69,7 +69,7 @@ while True:
     draw.rectangle((0, 0, width, 40), outline=0, fill="#ED4242")
     y = top
 
-    draw.text((x, y), "Height: " + height + ", Width: " + width, font=font, fill="#FFFFFF")
+    draw.text((x, y), "Height: " + str(height) + ", Width: " + str(width), font=font, fill="#FFFFFF")
 
     # create starting point
 
