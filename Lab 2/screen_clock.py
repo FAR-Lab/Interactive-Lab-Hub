83478f87785bb5f4b3daffa3f267efedafc07bb3
swillenson <shw58@cornell.edu>
2022-09-11 18:55:44 -0400

    adding test image

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -69,7 +69,22 @@ while True:
     y = top
     draw.text((x, y), timeNow, font=font, fill="#FFFFFF")
 
+    #adding image to be displayed
+    image = Image.open("testPic.jpg")
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
