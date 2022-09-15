fda70cec9e8fe5c15fd1dd9b7f2e9e768ee6d807
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-15 09:21:51 -0400

    fix str

--- Lab 2/rotary.py
+++ Lab 2/rotary.py
@@ -84,7 +84,7 @@ while True:
 
     # TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
     y = top
-    IP = "Proximity: " + sensor.proximity()
+    IP = "Proximity: " + str(sensor.proximity())
     draw.text((x, y), IP, font=font, fill="#FFFFFF")
 
     # Display image.
