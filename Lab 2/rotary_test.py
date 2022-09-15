30c9b97f09598c7c8ebb446c2dfca3450702ebf4
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-15 09:36:24 -0400

    add rotary_test.py

--- Lab 2/rotary_test.py
+++ Lab 2/rotary_test.py
@@ -10,5 +10,14 @@ apds = APDS9960(i2c)
 apds.enable_proximity = True
 
 while True:
-    print(apds.proximity)
+
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    y = top
+    IP = "Proximity: " + str(apds.proximity)
+    draw.text((x, y), IP, font=font, fill="#FFFFFF")
+
+    # Display image.
+    disp.image(image, rotation)
     time.sleep(0.2)
