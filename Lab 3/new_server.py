73e80ec2275b6b506c1822ae3190c204bc989d94
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-26 23:25:19 -0400

    update new_server

--- Lab 3/new_server.py
+++ Lab 3/new_server.py
@@ -1,12 +1,10 @@
-from flask import Flask
-from time import strftime, sleep
-test = strftime("%m/%d/%Y %H:%M:%S")
+from flask import Flask, Response,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return strftime("%m/%d/%Y %H:%M:%S")
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
