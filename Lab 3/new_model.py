d549eafb47cff0e01b5bc362893733ed03932ae0
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-26 23:10:41 -0400

    remove wav

--- Lab 3/new_model.py
+++ Lab 3/new_model.py
@@ -10,10 +10,10 @@ if not os.path.exists("model"):
     print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
     exit (1)
 
-wf = wave.open(sys.argv[1], "rb")
-if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-    print ("Audio file must be WAV format mono PCM.")
-    exit (1)
+# wf = wave.open(sys.argv[1], "rb")
+# if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    # print ("Audio file must be WAV format mono PCM.")
+    # exit (1)
 
 model = Model("model")
 # You can also specify the possible word list
