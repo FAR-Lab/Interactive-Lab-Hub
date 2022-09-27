39a6856c4eaa60c7632043ee881488658baf6ea2
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-26 22:54:50 -0400

    update new_model

--- Lab 3/new_model.py
+++ Lab 3/new_model.py
@@ -16,7 +16,7 @@ if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE
 
 model = Model("model")
 # You can also specify the possible word list
-rec = KaldiRecognizer(model, wf.getframerate(), '["oh one two three four five six seven eight nine zero", "[unk]"]')
+rec = KaldiRecognizer(model, wf.getframerate())
 
 while True:
     data = wf.readframes(4000)
