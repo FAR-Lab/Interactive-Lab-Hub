22176479dc21aea384c4902519098d549fdb3cb4
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-09-26 23:07:16 -0400

    adjust new_model

--- Lab 3/new_model.py
+++ Lab 3/new_model.py
@@ -4,6 +4,7 @@ from vosk import Model, KaldiRecognizer
 import sys
 import os
 import wave
+import pyaudio
 
 if not os.path.exists("model"):
     print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
@@ -16,10 +17,14 @@ if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE
 
 model = Model("model")
 # You can also specify the possible word list
-rec = KaldiRecognizer(model, wf.getframerate())
+rec = KaldiRecognizer(model, 16000)
+
+cap = pyaudio.PyAudio()
+stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
 
 while True:
-    data = wf.readframes(4000)
+    data = stream.read(4096)
     # if len(data) == 0:
         # break
     if rec.AcceptWaveform(data):
