1e08ae7b5a13eaf84cdd7f3bb4fcb55e63028ca1
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-10-02 16:16:10 -0400

    create light_switch.py

---
+++ Lab 3/light_switch.py
@@ -0,0 +1,120 @@
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
+
+# import qwiic_button library
+import qwiic_button
+
+from vosk import Model, KaldiRecognizer
+import sys
+import os
+import wave
+import json
+import pyaudio
+
+if not os.path.exists("model"):
+    print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
+    exit (1)
+
+# wf = wave.open(sys.argv[1], "rb")
+# if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    # print ("Audio file must be WAV format mono PCM.")
+    # exit (1)
+
+model = Model("model")
+# You can also specify the possible word list
+rec = KaldiRecognizer(model, 16000)
+
+cap = pyaudio.PyAudio()
+stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+message = ""
+
+# Add Qwiic button
+my_button = qwiic_button.QwiicButton()
+brightness = 100
+cycle_time = 1000
+off_time = 200
+
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((10, 5), "Live caption:", font=font, fill="#FFFFFF")
+    y = top
+
+    data = stream.read(4096)
+
+    if rec.AcceptWaveform(data):
+        res = json.loads(rec.Result())
+        message = res['text']
+        print(res['text'])
+        draw.text((10, 50), res['text'], font=font, fill="#FFFFFF")
+        if message == "on":
+            my_button.LED_on(brightness)
+        elif message == "off":
+            my_button.LED_off()
+    else:
+        draw.text((10, 50), message, font=font, fill="#FFFFFF")
+        
+    # Display image.
+    disp.image(image, rotation)
+    time.sleep(0.1)
