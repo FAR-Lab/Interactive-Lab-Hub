f7904e7a923421b85bbf56bde437996d3d44ef53
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-10-12 17:08:16 -0400

    insert scratch sound

--- Lab 4/test_music.py
+++ Lab 4/test_music.py
@@ -6,6 +6,8 @@ import pygame
 import time
 mixer.init() #Initialzing pyamge mixer
 
+song_1 = pygame.mixer.Sound('scratch.mp3')
+
 mixer.music.load('umeda.mp3') #Loading Music File
 
 mixer.music.play() #Playing Music with Pygame
@@ -18,12 +20,14 @@ def on_press(key):
     if key == keyboard.Key.space:
         if paused == True:     # time to play audio
             print ('play pressed')
-            pygame.mixer.music.unpause()
+            # pygame.mixer.music.unpause()
+            song_1.stop()
             paused = False
             return False
         elif paused == False:   # time to pause audio
             print ('pause pressed')
-            pygame.mixer.music.pause()
+            # pygame.mixer.music.pause()
+            song_1.play(0)
             paused = True
             return False
     return False
