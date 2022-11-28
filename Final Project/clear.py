3719db9ca75b582e4ac9db218531a4c9f886ff15
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-11-27 21:53:29 -0500

    create clear.py

---
+++ Final Project/clear.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple test for NeoPixels on Raspberry Pi
+import time
+import board
+import neopixel
+import digitalio
+import busio
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D21
+
+# The number of NeoPixels
+num_pixels = 30
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+# ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False
+)
+
+pixels.clear()
