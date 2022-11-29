23e223572919a8979d614a49965b0e17b206cdb0
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-11-29 11:37:32 -0500

    add modulus 30

--- Final Project/new_test.py
+++ Final Project/new_test.py
@@ -37,12 +37,12 @@ while True:
     for x in range (prev_index,index):
         prev_index = index
         for y in range (0,85):
-            pixels[x-3] = (85-y, 85-y, 85-y)
-            pixels[x-2] = (170-y, 170-y, 170-y)
-            pixels[x-1] = (255-y, 255-y, 255-y)
-            pixels[x] = (170+y, 170+y, 170+y)
-            pixels[x+1] = (85+y, 85+y, 85+y)
-            pixels[x+2] = (y, y, y)
+            pixels[(x%30)-3] = (85-y, 85-y, 85-y)
+            pixels[(x%30)-2] = (170-y, 170-y, 170-y)
+            pixels[(x%30)-1] = (255-y, 255-y, 255-y)
+            pixels[(x%30)] = (170+y, 170+y, 170+y)
+            pixels[(x%30)+1] = (85+y, 85+y, 85+y)
+            pixels[(x%30)+2] = (y, y, y)
             #Add 1 to the counter
             #Add a small time pause which will translate to 'smoothly' changing colour
             print(x)
