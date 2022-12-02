8da889cb5cef7e6fcd83dfc9e58531ca71191688
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-12-01 23:28:07 -0500

    random everything

--- Lab 4/tree_test.py
+++ Lab 4/tree_test.py
@@ -19,13 +19,13 @@ while True:
         servo.angle = 0
         time.sleep(randrange(20,200)/100)
         # Set the servo to 0 degree position
-        servo.angle = 50
+        servo.angle = randrange(20,50)
         time.sleep(randrange(20,100)/100)
-        servo.angle = 25
+        servo.angle = randrange(20,50)
         time.sleep(randrange(20,100)/100)
-        servo.angle = 50
+        servo.angle = randrange(20,50)
         time.sleep(randrange(20,100)/100)
-        servo.angle = 35
+        servo.angle = randrange(20,50)
         time.sleep(randrange(20,100)/100)
         servo.angle = 0
         time.sleep(randrange(20,200)/100)
