134e79a12ad88b609da0ef861796c5eb32cde968
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-12-05 19:56:14 -0500

    barebone

--- Final Project/ume.py
+++ Final Project/ume.py
@@ -1,17 +1,8 @@
-from pynput import keyboard
-
 #pip install pygame
 from pygame import mixer
-import pygame
-import time
-import board
-
-import busio
 
 mixer.init() #Initialzing pyamge mixer
-
 mixer.music.load('umeda.wav') #Loading Music File
-
 mixer.music.play() #Playing Music with Pygame
 
 
