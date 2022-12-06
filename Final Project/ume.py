80a6c685e19bca963c6c19a94d574fdf1c608218
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-12-05 20:06:14 -0500

    test

--- Final Project/ume.py
+++ Final Project/ume.py
@@ -1,9 +1,13 @@
 #pip install pygame
+import pygame
 from pygame import mixer
 
 mixer.init() #Initialzing pyamge mixer
 mixer.music.load('umeda.wav') #Loading Music File
 mixer.music.play() #Playing Music with Pygame
+
+while True:
+    mixer.music.play()
 
 
 
