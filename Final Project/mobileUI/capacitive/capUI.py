996735dc629735fb9792ab3390bb114f938daf39
Brian Mulyadi <mulyadi.brian@gmail.com>
2022-12-05 21:43:45 -0500

    update mobile ui

---
+++ Final Project/mobileUI/capacitive/capUI.py
@@ -0,0 +1,50 @@
+import time
+import board
+import os
+import RPi.GPIO as GPIO
+import pygame
+import adafruit_mpr121
+import busio
+import json
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+# Triggers capacitive sensor and sends out data
+def sensor_update_json(num):
+    if mpr121[1].value:
+        return (num, 1)
+    elif mpr121[2].value:
+        return (num, 2)
+    elif mpr121[3].value:
+        return (num, 3)
+    elif mpr121[4].value:
+        return (num, 4)
+    elif mpr121[5].value:
+        return (num, 5)
+    else: return (num, num)
+
+
+def main():
+    cur_index = 0
+
+    while True: 
+        
+        cur_index, triggered = sensor_update_json(cur_index)
+        if cur_index != triggered:
+            cur_index = triggered
+        json_value = cur_index
+        print(json_value)
+        time.sleep(0.5)
+        json_list = [{"layer":json_value}]
+        json_string = json.dumps(json_list)
+        json_file = open("data.json", "w")
+        json_file.write(json_string)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- Final Project/mobileUI/sketch.js
+++ Final Project/mobileUI/sketch.js
@@ -1,39 +1,146 @@
-var mode = 0 //Home Screen
-let bcFont
+let mode;
+let bcFont;
+let data;
+let smallPoint, largePoint;
+let showText = true;
+let i; //make image come out faster
+
 
 function preload(){
   bcFont = loadFont("Mattire.otf");
+  Hector = loadImage('Hector.png');
+  Imelda = loadImage("Imelda.png");
+  Julio = loadImage("Julio.png");
+  Rosita = loadImage("Rosita.png");
+  Victoria = loadImage("Victoria.png");
+  data = loadJSON("capacitive/data.json");
+
 }
 
 function setup() {
   createMetaTag();
-  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  createCanvas(window.innerWidth, window.innerHeight);
+  smallPoint = 4;
+  largePoint = 10;
+  Hector.loadPixels();
+  Imelda.loadPixels();
+  Julio.loadPixels();
+  Rosita.loadPixels();
+  Victoria.loadPixels();
+  noStroke();
+  background(255, 251, 232);
 }
+
+function startBackgound() {
+  background(146, 115, 98)
+}
+
 
 function draw() {
-  background(220);
-
+  mode = data[0].layer;
+  //translate(-width/2,-height/2,0);
   if (mode == 0) {
     home();
-  } else if (mode == 1) {
-    screen2();
-  } else if (mode == 2) {
-    screen3();
+  } else if (mode == 1){
+    hector();
   }
-  
+  else if (mode == 2) {
+    victoria();
+  }
+  else if (mode == 3) {
+    julio();
+  }
+  else if (mode == 4) {
+    rosita();
+}
+  else if (mode == 5) {
+    victoria();
+}
 }
 
+//Home Screen
 function home() {
-  textFont(bcFont, 20);
-  textSize(width / 15);
-  textAlign(CENTER, CENTER);
   background(255, 251, 232);
-  let time = millis();
-  rotateX(time / 1000);
-  rotateZ(time / 1234);
-  text('Afterlife Garden', 0, 0); 
-  fill(0, 102, 153);
+  textFont(bcFont, 20);
+  textSize(width / 10);
+  textAlign(CENTER,CENTER);
+  if (showText){
+    text('Afterlife Garden', width/2,height/2);
+    fill(51,45,33);
+  }
 }
+
+// End of Home Screen
+
+//Family Memeber 1: Hector
+function hector() {
+  //let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  for(i=0; i < 10; i++){
+    let x = floor(random(Hector.width));
+    let y = floor(random(Hector.height));
+    let Hpix = Hector.get(x, y);
+    fill(Hpix);
+    ellipse(x, y, 5, 5);
+  }
+}
+
+// End of Hector
+
+//Family Memeber 2: Imelda
+function imelda() {
+  //let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  Imelda.resize(width, 0)
+  for(i=0; i < 10; i++){
+    let x = floor(random(Imelda.width));
+    let y = floor(random(Imelda.height));
+    let Ipix = Imelda.get(x, y);
+    fill(Ipix);
+    ellipse(x, y, 5, 5);
+  }
+}
+// End of Imelda
+  
+//Family member3 Julio
+function julio() {
+  //let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  Julio.resize(width, 0)
+  for(i=0; i < 10; i++){
+    let x = floor(random(Julio.width));
+    let y = floor(random(Julio.height));
+    let Jpix = Julio.get(x, y);
+    fill(Jpix);
+    ellipse(x, y, 5, 5);
+  }
+}
+//End of Julio
+
+//Family member 4 Rosita
+function rosita() {
+  //let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  Rosita.resize(width, 0)
+  for(i=0; i < 10; i++){
+    let x = floor(random(Rosita.width));
+    let y = floor(random(Rosita.height));
+    let Rpix = Rosita.get(x, y);
+    fill(Rpix);
+    ellipse(x, y, 5, 5);
+  }
+}
+//End of Rosita
+
+//Family member 4 Rosita
+function victoria() {
+  //let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  Victoria.resize(width, 0)
+  for(i=0; i < 10; i++){
+    let x = floor(random(Victoria.width));
+    let y = floor(random(Victoria.height));
+    let Vpix = Victoria.get(x, y);
+    fill(Vpix);
+    ellipse(x, y, 5, 5);
+  }
+}
+//End of Victoria
 
 function createMetaTag() {
   let meta = createElement('meta');
@@ -43,3 +150,5 @@ function createMetaTag() {
   let head = select('head');
   meta.parent(head);
 }
+
+
