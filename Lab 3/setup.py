b410d3251571df38c9d075f6b8f33d030a5a15e0
HaukeCornell <hgs52@cornell.edu>
2023-09-19 12:44:25 +0200

    Update setup.py

--- Lab 3/setup.py
+++ Lab 3/setup.py
@@ -10,11 +10,23 @@ home_dir = os.path.expanduser("~")
 
 # Download the zip file
 zip_file_path = os.path.join(home_dir, "file.zip")
+
+print("Started downloading...")
 urllib.request.urlretrieve(url, zip_file_path)
+print("Finished downloading!")
 
 # Extract the contents of the zip file
+print("Started unzipping...")
 with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
     zip_ref.extractall(home_dir)
+print("Finished unzipping!")
+
+# Change the permissions of the files in the folder to executable
+extracted_folder = os.path.join(home_dir, os.path.basename(zip_file_path).replace(".zip", ""))
+for root, dirs, files in os.walk(extracted_folder):
+    for file in files:
+        os.chmod(os.path.join(root, file), 0o755)
 
 # Remove the zip file
 os.remove(zip_file_path)
+print("Zip file removed!")
