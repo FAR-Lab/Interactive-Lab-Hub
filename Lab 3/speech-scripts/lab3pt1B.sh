aa92932838a97ff06b7381f122d0806061dfdf85
GilbertoRuiz <ger83@cornell.edu>
2023-09-24 17:34:01 -0400

    Finished lab3 pt 1

--- Lab 3/speech-scripts/lab3pt1B.sh
+++ Lab 3/speech-scripts/lab3pt1B.sh
@@ -4,4 +4,4 @@ echo "Hello user! Can you please tell me how many pets you have?" | festival --t
 
 python3 /home/gilbertoe.ruiz/Interactive-Lab-Hub/Lab\ 3/speech-scripts/test_microphone.py -m en -f /home/gilbertoe.ruiz/Interactive-Lab-Hub/Lab\ 3/speech-scripts/lab3pt1B.txt
 
-python3 /home/gilbertoe.ruiz/Interactive-Lab-Hub/Lab\ 3/format_partial.py
+python3 /home/gilbertoe.ruiz/Interactive-Lab-Hub/Lab\ 3/speech-scripts/format_partial.py
