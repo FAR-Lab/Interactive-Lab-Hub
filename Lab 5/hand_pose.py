fabd14edbd1f799118b6ce9f17257a7ad0a50585
HaukeCornell <hgs52@cornell.edu>
2023-10-17 19:09:42 -0400

    Background process for music playback

--- Lab 5/hand_pose.py
+++ Lab 5/hand_pose.py
@@ -6,6 +6,17 @@ import math
 from ctypes import cast, POINTER
 import alsaaudio
 m = alsaaudio.Mixer(control='Speaker', cardindex=3)
+m.setvolume(5) 
+import subprocess
+
+def play_audio():
+    command = ['./loop_audio.sh']
+    process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print('Looping audio playback - press q to quit')
+    return process  # Return the process object
+
+audio_process = play_audio()
+
 ################################
 wCam, hCam = 640, 480
 ################################
@@ -83,6 +94,11 @@ while True:
     pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
                 1, (255, 0, 0), 3)
- 
+
     cv2.imshow("Img", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
+        audio_process.terminate()  #
+        break
+
+cap.release()
+cv2.destroyAllWindows()
